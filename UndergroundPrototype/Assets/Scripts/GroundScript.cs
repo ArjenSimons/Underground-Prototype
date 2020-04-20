a3aa4f5d689527d19fc20c1786bca8bb914930a6
Brent <brent.schreurs@hva.nl>
2020-04-20 14:23:07 +0200

    Added Ground/Wall functionality

---
+++ UndergroundPrototype/Assets/Scripts/GroundScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GroundType
+{
+    Regular = 0,
+    Iron,
+    Fuel,
+    Crystal
+}
+
+public class GroundScript : MonoBehaviour
+{
+
+    public class Ground
+    {
+        public GroundType type;
+
+        public Ground(GroundType groundType)
+        {
+            type = groundType;
+        }
+    }
+
+    public GroundType type;
+    public int iron;
+    public int fuel;
+    public int crystal;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        //Determine available resources and used textures per groundType
+        switch (type)
+        {
+            case GroundType.Regular:
+                break;
+            case GroundType.Iron:
+                iron = 100;
+                break;
+            case GroundType.Fuel:
+                fuel = 100;
+                break;
+            case GroundType.Crystal:
+                crystal = 50;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
---
+++ UndergroundPrototype/Assets/Scripts/WallScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum WallType
+{
+    Regular = 0,
+    Iron,
+    Fuel,
+    Crystal
+}
+
+public class WallScript : MonoBehaviour
+{
+    public class Wall
+    {
+        public WallType type;
+
+        public Wall(WallType wallType)
+        {
+            type = wallType;
+        }
+    }
+
+    public WallType type;
+    public int health;
+    private Renderer renderer;
+    private Color colour;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponentInChildren<Renderer>();
+        colour = renderer.material.color;
+        
+
+        health = 5;
+        
+        //Define textures etc. per wallType
+        switch (type)
+        {
+            case WallType.Regular:
+                break;
+            case WallType.Iron:
+                break;
+            case WallType.Fuel:
+                break;
+            case WallType.Crystal:
+                break;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (health)
+        {
+            case 5:
+                colour = renderer.material.color;
+                colour.a = 0.1f;
+                renderer.material.color = colour;
+                break;
+            case 4:
+                colour = renderer.material.color;
+                colour.a += 0.1f;
+                renderer.material.color = colour;
+                break;
+            case 3:
+                colour = renderer.material.color;
+                colour.a += 0.1f;
+                renderer.material.color = colour;
+                break;
+            case 2:
+                colour = renderer.material.color;
+                colour.a += 0.1f;
+                renderer.material.color = colour;
+                break;
+            case 1:
+                colour = renderer.material.color;
+                colour.a += 0.1f;
+                renderer.material.color = colour;
+                break;
+        }
+        if (health == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
