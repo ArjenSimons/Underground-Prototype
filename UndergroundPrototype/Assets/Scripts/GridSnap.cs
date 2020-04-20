2a894f7a052aae0e704e724fd3db95e7bb39369d
Arjen Simons <arjen.simons@gmail.com>
2020-04-20 15:06:08 +0200

    Fix merge error

---
+++ UndergroundPrototype/Assets/Scripts/GridSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GridSnap : MonoBehaviour
+{
+    [SerializeField] private float gridSize = 1;
+    private Vector3 truePos;
+
+    private void Update()
+    {
+        truePos.x = Mathf.Floor(transform.parent.position.x / gridSize) * gridSize;
+        truePos.y = Mathf.Floor(transform.parent.position.y / gridSize) * gridSize;
+        truePos.z = Mathf.Floor(transform.parent.position.z / gridSize) * gridSize;
+
+        transform.position = truePos;
+    }
+}
+
