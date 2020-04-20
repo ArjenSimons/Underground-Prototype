e1d8f32f8f812d65209875f72ffeefe2b1088587
DESKTOP\raymond <raymdelta@gmail.com>
2020-04-20 15:19:15 +0200

    Added resource manager script with changing text

---
+++ UndergroundPrototype/Assets/ResourceManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ResourceManager : MonoBehaviour
+{
+
+    private int fuel, iron, crystal;
+    public int Iron { get; set; }
+    public int Crystal { get; set; }
+    public int Fuel { get; set; }
+
+    [SerializeField] TextMeshProUGUI ironText;
+    [SerializeField] TextMeshProUGUI crystalText;
+    [SerializeField] TextMeshProUGUI fuelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        iron = 0;
+        fuel = 0;
+        crystal = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Text update is done every frame, text should only be set when a change has happened.
+        iron += 5;
+        ironText.SetText("{0}", iron);
+        fuel += 5;
+        fuelText.SetText("{0}", fuel);
+        crystal += 5;
+        crystalText.SetText("{0}", crystal);
+    }
+}
