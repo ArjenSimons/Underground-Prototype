46c1ec7602c624a7761589178e30b13785893495
Marijn <marijnbecking@gmail.com>
2020-04-21 14:15:07 +0200

    Added selection of units & Added mass/multiple unit move order

--- UndergroundPrototype/Assets/InputHandler.cs
+++ UndergroundPrototype/Assets/InputHandler.cs
@@ -6,15 +6,32 @@ using UnityEngine;
 public class InputHandler : MonoBehaviour
 {
 
-    Camera mainCam;
-    GameObject g;
+    private Camera mainCam;
+    private GameObject unitSelector;
+    private Vector3 dragStartPos = new Vector3(0,0,0);
+    private BoxCollider tempShowBoxCollider;
+
+    private List<GameObject> allUnits = new List<GameObject>();
+    public GameObject AllUnits
+    {
+        //get { return selectedUnits; }
+        set { allUnits.Add(value); }
+    }
+    private List<GameObject> selectedUnits = new List<GameObject>();
+    private bool selectionStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCam = this.GetComponent<Camera>();
-        g = new GameObject();
-        Instantiate(g, transform.position, Quaternion.identity);
+        unitSelector = GameObject.Find("UnitSelector"); // prefab?
+        //Instantiate(g, transform.position, Quaternion.identity);
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        if (tempShowBoxCollider) { Gizmos.DrawCube(tempShowBoxCollider.center, tempShowBoxCollider.size); }
     }
 
     // Update is called once per frame
@@ -26,12 +43,29 @@ public class InputHandler : MonoBehaviour
 
     void HandleInput()
     {
+        
         if (Input.GetAxis("Fire2") > 0)
         {
             Vector3 pos = SelectPositon(Input.mousePosition);
             // instantiate event?
 
-            g.transform.position = pos;
+            Order(pos);
+
+            unitSelector.transform.position = pos;
+        }
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            //Debug.Log("selecting");
+            CreateUnitSelection(Input.mousePosition);
+        } else
+        {
+            // mouse was let go, reset
+            if (selectionStarted)
+            {
+                CreateSelection();
+            }
+            selectionStarted = false;
+            // CollectUnits(); store units in list?
         }
     }
 
@@ -45,14 +79,68 @@ public class InputHandler : MonoBehaviour
         
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            Debug.Log(LayerMask.LayerToName(hit.collider.gameObject.layer));
+           // Debug.Log(LayerMask.LayerToName(hit.collider.gameObject.layer));
             Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+            Debug.Log(hit.point.y);
             return hit.point;
         }
         else
         {
             Debug.DrawRay(ray.origin, ray.direction * 100, Color.white);
+            Debug.Log("Failed to hit surface");
         }
         return new Vector3(0,0,0);
+    }
+
+    private void CreateUnitSelection(Vector2 mousePos)
+    {
+        Vector3 dragEndPos = SelectPositon(mousePos);
+        if (!selectionStarted)
+        {
+            dragStartPos = SelectPositon(mousePos); // only reset upon mouse release
+           // Debug.Log(dragStartPos);
+            selectionStarted = true;
+        }
+        unitSelector.transform.position = new Vector3(dragEndPos.x, dragStartPos.y, dragEndPos.z);
+
+        BoxCollider bc = unitSelector.GetComponent<BoxCollider>();
+        tempShowBoxCollider = bc;
+
+        
+
+        float xDistance = dragStartPos.x - dragEndPos.x;
+        float yPos = bc.size.y / 2;
+        float zDistance = dragStartPos.z - dragEndPos.z;
+
+        bc.size = new Vector3(Mathf.Abs(xDistance), bc.size.y, Mathf.Abs(zDistance));
+        bc.center = new Vector3(xDistance/2, yPos, zDistance/2);
+
+        //Debug.Log(Vector3.Distance(dragStartPos, dragEndPos));
+        //Debug.Log(dragStartPos.z - dragEndPos.z);
+    }
+
+    private void CreateSelection()
+    {
+        BoxCollider bc = unitSelector.GetComponent<BoxCollider>();
+        selectedUnits.Clear();
+        for (int i = 0; i < allUnits.Count; i++)
+        {
+            if (bc.bounds.Contains(allUnits[i].transform.position))
+            {
+                allUnits[i].GetComponent<UnitBehavior>().Select();
+                selectedUnits.Add(allUnits[i]);
+            } else
+            {
+                allUnits[i].GetComponent<UnitBehavior>().Deselect();
+            }
+        }
+    }
+
+    private void Order(Vector3 pos /*, structureTypeEnum? structure.enemy, structure.build, structure.repair enz.*/ )
+    {
+        for (int i = 0; i < selectedUnits.Count; i++)
+        {
+            selectedUnits[i].GetComponent<UnitBehavior>().DoInvoke(new UnitDataEventArgs(this, "Move", pos));
+        }
     }
 }
--- UndergroundPrototype/Assets/UnitBehavior.cs
+++ UndergroundPrototype/Assets/UnitBehavior.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [System.Serializable]
-public class UnitPerformAction : UnityEvent<string>
-{
-
-}
+public class UnitPerformAction : UnityEvent<UnitDataEventArgs> { } // override to use type
 
 public class UnitBehavior : MonoBehaviour
 {
     //Dictionary<string, int> actionList = new Dictionary<string, int>();
+    //public void DataTemp(Vector3 pos, GameObject gameObj)
+    //{
+    //    Vector3 posData = pos;
+    //    GameObject gameObjData = gameObj;
+    //}
+    private Vector3 moveOrder = new Vector3(0,0,0);
 
     enum ActionList
     {
@@ -29,8 +32,10 @@ public class UnitBehavior : MonoBehaviour
         //actionList.Add("Move", 0);
         //actionList.Add("Attack", 1);
         //actionList.Add("Hold", 2);
+        InputHandler inputHandler = Camera.main.GetComponent<InputHandler>();
+        inputHandler.AllUnits = this.gameObject; // adds oneself to list of inputhandler
 
-     if (unitEvent == null) { unitEvent = new UnitPerformAction(); }
+        if (unitEvent == null) { unitEvent = new UnitPerformAction(); }
         unitEvent.AddListener(CallForAction);
     }
 
@@ -44,32 +49,90 @@ public class UnitBehavior : MonoBehaviour
     {
         if (currentAction == 0)
         {
+            //unitEvent.Invoke("Move", );
             // holding postition
             return;
         }
         if (currentAction == 1)
         {
-            //MoveToPosition();
+            MoveUnitToPosition(moveOrder);
         }
         if (currentAction == 2)
         {
-            //AttackSelectedUnit();
+            AttackSelectedUnit(new GameObject());
         }
     }
 
-    void CallForAction(string action)
+    public void DoInvoke(UnitDataEventArgs args)
     {
-        switch (action)
+        unitEvent.Invoke(args);
+    }
+
+    void CallForAction(UnitDataEventArgs data)
+    {
+        Debug.Log(data.pos);
+        Debug.Log("action is coming");
+        switch (data.action)
         {
             case "Move":
-                currentAction = (int)ActionList.Move;
+                moveOrder = data.pos;
+                currentAction = (int) ActionList.Move;
                 break;
             case "Attack":
-                currentAction = (int)ActionList.Attack;
+                currentAction = (int) ActionList.Attack;
                 break;
             case "Hold":
-                currentAction = (int)ActionList.Hold;
+                currentAction = (int) ActionList.Hold;
                 break;
         }
     }
+
+    void MoveUnitToPosition(Vector3 selectedPos)
+    {
+        float step = 5 * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(selectedPos.x, transform.position.y, selectedPos.z), step);
+        //Debug.Log(transform.position + " / " + selectedPos);
+        if (Vector3.Distance(transform.position, selectedPos) < .5f)
+        {
+            currentAction = (int) ActionList.Hold;
+            Debug.Log("Resetting action to hold");
+        }
+    }
+
+    void AttackSelectedUnit(GameObject enemy)
+    {
+
+    }
+
+    public void Select()
+    {
+        this.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void Deselect()
+    {
+        this.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
+
+public struct UnitDataEventArgs
+{
+    public object sender;
+    public string action;
+    public Vector3 pos;
+    //public GameObject enemyUnit;
+
+    public UnitDataEventArgs (object sender, string action, Vector3 pos)
+    {
+        this.sender = sender;
+        this.action = action; 
+        this.pos = pos; 
+    }
+
+    //public UnitDataEventArgs(object sender, string action, GameObject enemyUnit)
+    //{
+    //    this.sender = sender;
+    //    this.action = action;
+    //    this.enemyUnit = enemyUnit;
+    //}
 }
