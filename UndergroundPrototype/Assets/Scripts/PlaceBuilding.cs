25722d7f26003d1720e6cdd5dc5bb3d339a6da09
DESKTOP\raymond <raymdelta@gmail.com>
2020-04-21 11:40:34 +0200

    Added building placing positioning using the InputHandler SelectPosition function

--- UndergroundPrototype/Assets/Scripts/PlaceBuilding.cs
+++ UndergroundPrototype/Assets/Scripts/PlaceBuilding.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 public class PlaceBuilding : MonoBehaviour
 {
 
-    [SerializeField]
-    GameObject mine;
-
-    private void OnEnable()
-    {
-        
-    }
+    public bool placingBuilding;
+    InputHandler inputHandler;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        placingBuilding = true;
+        inputHandler = GameObject.Find("Main Camera").GetComponent<InputHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (placingBuilding == true) {
+            Vector3 raycastPlacePos = inputHandler.SelectPositon(Input.mousePosition);
+            //Force height at ground level
+            raycastPlacePos.y = 1;
+            this.transform.position = raycastPlacePos;
+            }
     }
 }
