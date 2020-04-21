0b83138eb63210d7d868e060590e8fcbbaf93789
Arjen Simons <arjen.simons@gmail.com>
2020-04-21 11:49:33 +0200

    Added a structure health script

---
+++ UndergroundPrototype/Assets/Scripts/BaseStationHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StructureHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+    public UnityEvent buildingIsDestroyed;
+
+    public void DamageBuilding(int damage)
+    {
+        health -= damage;
+        
+        if (health <= 0)
+        {
+            buildingIsDestroyed.Invoke();
+        }
+    }
+}
