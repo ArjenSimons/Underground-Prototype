bb674ce2f133c99532c0ee2435c4e6d461151633
DESKTOP\raymond <raymdelta@gmail.com>
2020-04-21 10:15:56 +0200

    Added placebuilding

---
+++ UndergroundPrototype/Assets/Scripts/PlaceBuilding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceBuilding : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject mine;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
