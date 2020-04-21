05fb885ce944036e7afb00c84fe2ec934057de18
DESKTOP\raymond <raymdelta@gmail.com>
2020-04-21 13:36:49 +0200

    Added material change during placement mode to display placement location

--- UndergroundPrototype/Assets/Scripts/PlaceBuilding.cs
+++ UndergroundPrototype/Assets/Scripts/PlaceBuilding.cs
@@ -7,10 +7,15 @@ public class PlaceBuilding : MonoBehaviour
 
     public bool placingBuilding;
     InputHandler inputHandler;
+    Material opaqueMaterial;
+    Material transparentMaterial;
+
+    private bool allowMaterialChange = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        transparentMaterial = Resources.Load<Material>("BuildingAlpha");
         placingBuilding = true;
         inputHandler = GameObject.Find("Main Camera").GetComponent<InputHandler>();
     }
@@ -19,10 +24,25 @@ public class PlaceBuilding : MonoBehaviour
     void Update()
     {
         if (placingBuilding == true) {
+            //Make transparant in placement mode
+            print(this.gameObject.GetComponentInChildren<Renderer>().gameObject.name);
+            opaqueMaterial = this.gameObject.GetComponentInChildren<Renderer>().material;
+            this.gameObject.GetComponentInChildren<Renderer>().material = transparentMaterial;
+
             Vector3 raycastPlacePos = inputHandler.SelectPositon(Input.mousePosition);
             //Force height at ground level
             raycastPlacePos.y = 1;
             this.transform.position = raycastPlacePos;
             }
+        else
+        {
+            if(allowMaterialChange == true)
+            {
+                //Remove transparancy on placement
+                this.gameObject.GetComponentInChildren<Renderer>().material = opaqueMaterial;
+                //Make sure the material is set once
+                allowMaterialChange = false;
+            }
+        }
     }
 }
