78a495d8a44b2ac6a73b7c856657949c49149449
Marijn <marijnbecking@gmail.com>
2020-04-22 13:39:52 +0200

    Added new features from rts-2's camera script

--- UndergroundPrototype/Assets/Scripts/CameraMovement.cs
+++ UndergroundPrototype/Assets/Scripts/CameraMovement.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
+
 public class CameraMovement : MonoBehaviour
 {
 
     private Camera playerCamera;
     private Transform cameraTransform;
     private Vector3 tempTransform;
-    private float standardCameraHeight;
-
+    private float cameraDistance = 0f;
+    private float cameraAngle = 0f;
+    private float cameraRotation = 0f;
     
-    private float cameraMinZoom = 0f;
     [Header("Camera Settings")]
-    [Range(0, 10)]
-    public float cameraMaxZoom = 10f;
-    [Range(1,10)]
-    public float cameraSensitivity = 6f;
+    [SerializeField]
+    private bool enableQERotation = true;
+    [SerializeField]
+    private bool enableMouse3Rotation = true;
+
+    [Header("Camera Zoom settings")]
+    [SerializeField]
+    [Range(0, 50)]
+    private float cameraDistanceDefault = 6f;
+    [SerializeField]
+    [Range(0, 50)]
+    private float cameraMinZoom = 0f;
+    [Range(0, 50)]
+    public float cameraMaxZoom = 50f;
+    [Range(1, 10)]
+    public float cameraZoomSpeed = 6f;
 
     [Header("Camera Movement Settings")]
     [Range(0f, 1f)]
@@ -27,12 +39,27 @@ public class CameraMovement : MonoBehaviour
     [Range(1, 50)]
     public int cameraScreenPercentage = 10;
 
+    [Header("Camera Rotational Settings")]
+    [SerializeField]
+    private float cameraAngleMin = 40f;
+    [SerializeField]
+    private float cameraAngleMax = 65f;
+    [SerializeField]
+    private float cameraAngleDefault = 60f;
+    [SerializeField]
+    private float camAngleSensitivity = 1f;
+    [SerializeField]
+    private float camRotationDelta = 0.5f;
+    [SerializeField]
+    private float camRotationSensitivity = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        playerCamera = this.GetComponent<Camera>();
+        playerCamera = this.GetComponentInChildren<Camera>();
         cameraTransform = playerCamera.transform;
-        standardCameraHeight = cameraTransform.position.y;
+        cameraDistance = cameraDistanceDefault;
+        cameraAngle = cameraAngleDefault;
     }
 
     // Update is called once per frame
@@ -46,37 +73,49 @@ public class CameraMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            Scroll(-cameraZoomSpeed);
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            Scroll(cameraZoomSpeed);
+        }
 
         cameraTransform.position += GetMouseDirection();
 
-        if (horizontal > cameraSpeedDeadZone) { cameraTransform.position += Vector3.right * cameraSpeed; }
-        else if (horizontal < -cameraSpeedDeadZone) { cameraTransform.position += Vector3.left * cameraSpeed; }
+        if (horizontal > cameraSpeedDeadZone) { Pan(Vector3.right); }
+        else if (horizontal < -cameraSpeedDeadZone) { Pan(Vector3.left); }
 
-        if (vertical > cameraSpeedDeadZone) { cameraTransform.position += Vector3.forward * cameraSpeed; }
-        else if (vertical < -cameraSpeedDeadZone) { cameraTransform.position += Vector3.back * cameraSpeed; }
+        if (vertical > cameraSpeedDeadZone) { Pan(Vector3.forward); }
+        else if (vertical < -cameraSpeedDeadZone) { Pan(Vector3.back); }
 
-        if (scrollWheel != 0f)
+
+        if (enableMouse3Rotation)
         {
-            float distance = Vector3.Distance(tempTransform, cameraTransform.position);
-            //Debug.Log(distance);
-
-            if (distance < cameraMinZoom && scrollWheel < 0f) { return; }
-            if (distance > cameraMaxZoom && scrollWheel > 0f) { return; }
-
-            Vector3 nextCameraPos = cameraTransform.forward * cameraSensitivity * scrollWheel;
-            cameraTransform.position += nextCameraPos;
-
-            if (cameraTransform.position.y > standardCameraHeight) { cameraTransform.position = new Vector3(cameraTransform.position.x,tempTransform.y,cameraTransform.position.z); }
-            //float fov = playerCamera.fieldOfView;
-            //fov += -scrollWheel * sensitivity;
-            //fov = Mathf.Clamp(fov, minFov, maxFov);
-            //playerCamera.fieldOfView = fov;
+            if (Input.GetMouseButton(2))
+            {
+                Rotate(Input.GetAxis("Mouse X") * camRotationSensitivity * (1));
+                Tilt(Input.GetAxis("Mouse Y") * camAngleSensitivity * (-1));
+            }
         }
-        else if (cameraTransform.position.y == standardCameraHeight)
+
+        if (enableQERotation)
         {
-            tempTransform = cameraTransform.position;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                Rotate(camRotationDelta * (1));
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                Rotate(camRotationDelta * (-1));
+            }
         }
+
+        UpdateCamera();
     }
 
     private Vector3 GetMouseDirection()
@@ -85,7 +124,6 @@ public class CameraMovement : MonoBehaviour
         // some screen percentage
         float widthPercentage = (float)Screen.width * (cameraScreenPercentage/100f);
         float heightPercentage = (float)Screen.height * (cameraScreenPercentage/100f);
-        //Debug.Log(widthPercentage + " "+ heightPercentage);
 
         Vector3 dirTransform = new Vector3(0,0,0);
         Vector2 mousePos = Input.mousePosition;
@@ -97,5 +135,35 @@ public class CameraMovement : MonoBehaviour
         if (mousePos.y < Screen.height && mousePos.y > Screen.height - heightPercentage) { dirTransform += Vector3.forward * cameraSpeed; }
 
         return dirTransform;
+    }
+
+    private void UpdateCamera()
+    {
+        Vector3 camPos = new Vector3(0f, Mathf.Sin(cameraAngle * Mathf.PI / 180) * cameraDistance, -(Mathf.Cos(cameraAngle * Mathf.PI / 180) * cameraDistance));
+        playerCamera.transform.localPosition = camPos;
+        playerCamera.transform.LookAt(transform);
+    }
+
+    private void Scroll(float distanceDelta)
+    {
+        cameraDistance += distanceDelta;
+        cameraDistance = Mathf.Clamp(cameraDistance, cameraMinZoom, cameraMaxZoom);
+    }
+
+    private void Rotate(float rotationDelta)
+    {
+        cameraRotation += rotationDelta;
+        transform.rotation = Quaternion.Euler(0f, cameraRotation, 0f);
+    }
+
+    private void Tilt(float angleDelta)
+    {
+        cameraAngle += angleDelta;
+        cameraAngle = Mathf.Clamp(cameraAngle, cameraAngleMin, cameraAngleMax);
+    }
+
+    private void Pan(Vector3 direction)
+    {
+        transform.Translate(direction*cameraSpeed, Space.Self);
     }
 }
