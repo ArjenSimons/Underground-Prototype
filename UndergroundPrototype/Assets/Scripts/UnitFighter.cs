5dd105025cb7824033c2ba3da6f06ec153cf9d75
Marijn <marijnbecking@gmail.com>
2020-04-23 13:55:51 +0200

    Added todo list

--- UndergroundPrototype/Assets/Scripts/UnitFighter.cs
+++ UndergroundPrototype/Assets/Scripts/UnitFighter.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class UnitFighter : MonoBehaviour
 {
+    private UnitBehavior ub;
     // Start is called before the first frame update
     void Start()
     {
 
+        Debug.Log("UnitFighter");
     }
 
     // Update is called once per frame
@@ -19,7 +21,20 @@ public class UnitFighter : MonoBehaviour
 
     public void DoAction(UnitBehavior self)
     {
-        Debug.Log("UnitFighter");
         self.DoInvoke(new UnitDataEventArgs(this, "Hold", Vector3.zero)); // use this function to stop unit (when done for example)
+
+        this.
+    }
+
+    private void OnInvoke()
+    {
+
+
+
+        // TODO:
+        // MOVE TO VICINITY OF UNIT
+        // HOLD POSITION
+        // ATTACK UNIT
+        // DEALSOMEDAMAGE();
     }
 }
