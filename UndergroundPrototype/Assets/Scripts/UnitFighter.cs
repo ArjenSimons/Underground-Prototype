128bf20989f25efa697e75b05c23be0186effd43
Marijn <marijnbecking@gmail.com>
2020-04-23 15:34:19 +0200

    Added movement & Added fog of war to unit

--- UndergroundPrototype/Assets/Scripts/UnitFighter.cs
+++ UndergroundPrototype/Assets/Scripts/UnitFighter.cs
@@ -21,8 +21,33 @@ public class UnitFighter : MonoBehaviour
 
     public void DoAction(UnitBehavior self)
     {
-        self.DoInvoke(new UnitDataEventArgs(this, "Hold", Vector3.zero)); // use this function to stop unit (when done for example)
+        //self.DoInvoke(new UnitDataEventArgs(this, "Hold", Vector3.zero)); // use this function to stop unit (when done for example)
+        GameObject target = self.SelectionData.selectedObject;
+
+        //if (LayerMask.LayerToName(target.layer) == "Enemy" /*add tag support*/)
+        //{
+            AttackTarget(target);
+        //} //
+
+        //self.Move
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if (LayerMask.LayerToName(other.gameObject.layer) == "Enemy")
+        //{
+        if (other.gameObject.name == "UnitFighter(clone)")
+        {
+            Debug.Log(other.gameObject.name);
+        }
+        //}
+    }
+
+    private void AttackTarget(GameObject enemy)
+    {
         
+        MoveToTarget(enemy.transform.position);
+
     }
 
     private void OnInvoke()
@@ -35,5 +60,13 @@ public class UnitFighter : MonoBehaviour
         // HOLD POSITION
         // ATTACK UNIT
         // DEALSOMEDAMAGE();
+    }
+
+    private void MoveToTarget(Vector3 enemyPos)
+    {
+        
+        float step = 5 * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(enemyPos.x, transform.position.y, enemyPos.z), step);
+        
     }
 }
