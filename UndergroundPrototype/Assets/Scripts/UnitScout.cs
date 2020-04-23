0cb31b043d78399ff3922279de764efcee11df2d
Brent <brent.schreurs@hva.nl>
2020-04-23 14:44:42 +0200

    Add scout functionality without rotating plane

--- UndergroundPrototype/Assets/Scripts/UnitScout.cs
+++ UndergroundPrototype/Assets/Scripts/UnitScout.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UnitScout : MonoBehaviour
 {
+    public Vector3 relativePos;
+    public GameObject scoutField;
+    int rotationCalc;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +16,13 @@ public class UnitScout : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        relativePos = Input.mousePosition - transform.position;
     }
 
     public void DoAction(UnitBehavior self)
     {
         Debug.Log("UnitScout");
+        Instantiate(scoutField, transform.position, Quaternion.Euler(0,0,0));
         self.DoInvoke(new UnitDataEventArgs(this, "Hold", Vector3.zero)); // use this function to stop unit (when done for example)
     }
 }
