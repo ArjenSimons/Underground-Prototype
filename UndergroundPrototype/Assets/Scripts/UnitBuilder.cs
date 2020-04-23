d10fb248ed75b77d24d3abc474e576eedb7a1e30
DESKTOP\raymond <raymdelta@gmail.com>
2020-04-23 19:16:38 +0200

    Added UnitBuilder script

--- UndergroundPrototype/Assets/Scripts/UnitBuilder.cs
+++ UndergroundPrototype/Assets/Scripts/UnitBuilder.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class UnitBuilder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float range;
+    [SerializeField] private float hitTime = 2;
+    private float timer = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    private int iron, fuel, crystal;
 
     public void DoAction(UnitBehavior self)
     {
-        Debug.Log("UnitBuilder");
+
+        print(self.SelectionData.selectedObject.tag);
+        if (self.SelectionData.selectedObject != null && self.SelectionData.selectedObject.tag == "Resource")
+        {
+            if (Vector3.Distance(transform.position, self.SelectionData.selectedObject.transform.position) > range)
+            {
+                //Walk towards point
+                Debug.Log("walking");
+                self.MoveUnitToPosition(self.SelectionData.pos);
+            }
+            else
+            {
+                //Collect resource
+                Debug.Log("destination reached");
+            }
+            Debug.Log("action on wall");
+        }
+        else
+        {
+            //Debug.Log(self.SelectionData.selectedObject.tag);
+            Debug.Log("cancel action");
+            StopAction(self);
+        }
+    }
+
+    private void StopAction(UnitBehavior self)
+    {
+        timer = 0;
         self.DoInvoke(new UnitDataEventArgs(this, "Hold", Vector3.zero)); // use this function to stop unit (when done for example)
     }
 }
